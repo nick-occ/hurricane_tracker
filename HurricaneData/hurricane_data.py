3580761f2f6fcc9989ece38b1c591d242c30935d
nick-occ <nicholaso1980@gmail.com>
2017-07-14 00:11:59 -0400

    changed python to create json files for each year

--- HurricaneData/hurricane_data.py
+++ HurricaneData/hurricane_data.py
@@ -13,41 +13,42 @@ for i in range(1851,2017):
     with open("%s_%s.json" % (output,i), "w") as _outfile:
         _outfile.write('')
 
-with open("%s.json" % (output), "w") as _outfile:
-        _outfile.write('')
-
 start_year = None
 
 with open(data,"r") as _file:
-        for f in _file:
-            data_list = [i.strip() for i in str(f).split(',')]
-            data_list.pop()
-            if re.search('^AL+\w*', data_list[0]) != None:
-                hurricanes["data"].append({"id": data_list[0],
-                                   "name": data_list[1],
-                                   "locations": data_list[2],
-                                   "location_data": []
-                                   })
-                hurr_len = len(hurricanes["data"])
+    for f in _file:
+        data_list = [i.strip() for i in str(f).split(',')]
+        data_list.pop()
+        if re.search('^AL+\w*', data_list[0]) != None:
+            hurricanes["data"].append({"id": data_list[0],
+                                "name": data_list[1],
+                                "locations": data_list[2],
+                                "location_data": []
+                                })
+            hurr_len = len(hurricanes["data"])
 
-            else:
-                if start_year == None:
-                    start_year = data_list[0][:4]
+        else:
+            if start_year == None:
+                start_year = data_list[0][:4]
                     
-                hurricanes["data"][(hurr_len - 1)*1]["location_data"].append({
-                    "date": data_list[0],
-                    "time": data_list[1],
-                    "storm_type": data_list[3],
-                    "lat": data_list[4][:-1],
-                    "lon": data_list[5],
-                    })
+            hurricanes["data"][(hurr_len - 1)*1]["location_data"].append({
+                "date": data_list[0],
+                "time": data_list[1],
+                "storm_type": data_list[3],
+                "lat": data_list[4][:-1],
+                "lon": data_list[5],
+                })
 
-                if start_year != None and len(hurricanes["data"][(hurr_len - 1)*1]["location_data"]) == int(hurricanes["data"][(hurr_len - 1)*1]["locations"]):
-                    start_year = None
+            if start_year != None and len(hurricanes["data"][(hurr_len - 1)*1]["location_data"]) == int(hurricanes["data"][(hurr_len - 1)*1]["locations"]):
+                start_year = None
 
-with open("%s.json" % (output) ,"a") as _outfile:
-    json.dump(hurricanes,_outfile)
-    
+for i in range(1851,2017):
+    curr_hurr = [h for h in hurricanes["data"] if h["id"][-4:] == str(i)]
+    print(curr_hurr)
+    with open("%s_%s.json" % (output,i), "w") as _outfile:
+        _outfile.write(json.dumps(curr_hurr))
+
+
                     
 
 
